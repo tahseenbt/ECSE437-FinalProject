35ee06cbd1463c5d386aa9600363457571da8947
triantosnick1122 <nicholas.triantos@cs.mcgill.ca>
2022-12-06 21:24:00 -0500

    add furnitureItem
    Change-Id: Ifb4935314ada8af4ba0d9252899694030a49b1fe

---
+++ src/main/java/com/ecse437/FinalProject/model/FurnitureItem.java
@@ -0,0 +1,34 @@
+package com.ecse437.FinalProject.model;
+
+public class FurnitureItem extends InventoryItem{
+
+    private String material;
+    private String dimensions;
+
+    public FurnitureItem(String pDescription, String pBrandName, String pProductName, double pPrice, String pMaterial, String pDimensions) {
+        super(pDescription, pBrandName, pProductName, pPrice);
+        material = pMaterial;
+        dimensions = pDimensions;
+    }
+
+    public FurnitureItem()
+    {
+        super();
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getDimensions() {
+        return dimensions;
+    }
+
+    public void setDimensions(String dimensions) {
+        this.dimensions = dimensions;
+    }
+}
